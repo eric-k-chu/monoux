5e293c37862b81e4c32494a7a66fa8b16a317e7d
Eric Chu <Kiyuen88ec@gmail.com>
2024-09-01 17:33:15 -0700

    added vert-web

---
+++ apps/vert-web/src/App.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react'
+
+export default function App(): ReactElement {
+  return (
+    <div className='flex min-h-screen items-center justify-center'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
